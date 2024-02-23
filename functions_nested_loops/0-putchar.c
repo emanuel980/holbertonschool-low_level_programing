4446cf24f01985bf12fe50c1eb442a619d7a9843
Emanuel <emednedezdelgado22>
2024-02-23 12:33:22 -0400

    task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Write a program that prints putchat, followed by a new*
+ * Return: On success 0.
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
