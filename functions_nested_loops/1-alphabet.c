c9207309dc63f75e6f7b5663ed6ed75cf377d1ad
Emanuel <emednedezdelgado22>
2024-02-23 12:40:40 -0400

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints 10 times the alphabet
+ *
+ * Return: void(success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+}
